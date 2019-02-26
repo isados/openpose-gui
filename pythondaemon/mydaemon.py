b041371129a973f5e9a817f9b445e48bffe08e89
isa <isa.beec.14@acharya.ac.in>
2019-02-26 13:20:22 +0300

    revert this commit; when working with openpose

--- pythondaemon/mydaemon.py
+++ pythondaemon/mydaemon.py
@@ -2,7 +2,7 @@ from pythondaemon.daemon import Daemon
 import sys
 import os
 
-init_script="docker_start"
+init_script="pid"
 
 class MyDaemon(Daemon):
     def __init__(self, *args, **kwargs):
@@ -17,8 +17,8 @@ class MyDaemon(Daemon):
         if os.path.isfile(f"{init_script}.pid"):
             with open(f"{init_script}.pid") as pidfile:
                 pid=pidfile.readline().strip()
-                # os.system(f"kill -9 {pid}")
-                os.system(f"docker stop {pid}")
+                os.system(f"kill -9 {pid}")
+                # os.system(f"docker stop {pid}")
                 os.unlink(f"./{init_script}.pid")
                 self.stop()
         else: print("Process has not started!")
